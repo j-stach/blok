dfa8d8523405a3b13611dec45b9215a096e47e39
jordan <jordan.stachura@gmail.com>
2024-06-19 14:12:42 -0400

    examples progress

--- examples/1.rs
+++ examples/1.rs
@@ -1,82 +1,50 @@
 
 /* Example to show basic usage and features */
 
-use blok::{
-    Layout, layout,
-    build::{ Block, Layer, Stack },
-};
+use blok::{ Block, Stack, layout };
 
-
-/// Block is the element type for the matrix.
-#[derive(Clone)]
 struct MyBlock {
-    // It can have other types too, but Blok will ignore them.
-    /// MyBlock contains its Props directly.
-    props: Option<MyProps>
+    id: String,
+    connections: Vec<String>
 } impl MyBlock {
-    fn new() -> Self { Self { props: Some(MyProps) }}
+    fn new(id: String) -> Self {
+        MyBlock { id, connections: Vec::new() }
+    }
 }
-impl Block<MyProps> for MyBlock {
-    // Boilerplate. Derive soon.
-    type Constructor = fn() -> MyBlock; // TODO BuildArgs type
-    fn properties(&self) -> &Option<MyProps> { &self.props }
-    fn properties_mut(&mut self) -> &mut Option<MyProps> { &mut self.props }
-    fn void() -> MyBlock { MyBlock { props: None }}
-}
-
-/// Stack is an array of Layers, therefore a 3-D matrix of Blocks.
-#[derive(Clone)]
-struct MyStack {
-    layers: Vec<MyLayer>
-}
-impl Stack<MyProps, MyBlock, MyLayer> for MyStack {
-    fn new() -> Self { Self { layers: Vec::new() }}
-    fn layers(&self) -> &Vec<MyLayer> { &self.layers }
-    fn layers_mut(&mut self) -> &mut Vec<MyLayer> { &mut self.layers }
-}
-
-/// Let's build a Pyramid.
-fn main() {
-    // Populate a layer using a constructor.
-    // TODO BuildArgs type will be added to this constructor!!
-    let mut layer1 = MyLayer::new();
-    let layout1 = layout![7; 7];
-    let new_block = MyBlock::new;
-    layer1.populate(layout1, new_block);
-
-    // Populate a layer with identical Blocks.
-    let mut layer2 = MyLayer::new();
-    let proto_block = MyBlock::new();
-    layer2.populate_with_clones(layout![5; 5], &proto_block);
-
-    // We'll get a little funky with this one.
-    let mut layer3 = MyLayer::new();
-    let new = MyBlock::new;
-    layer3.populate(layout![3], new_block);
-    layer3.add_row(vec![new(); 3]);
-    layer3.new_row();
-    layer3.add_block(new());
-    layer3.add_block(new());
-    layer3.insert_block(1, 0, new()).unwrap();
-
-    // Please be careful if you do this. The layout and blocks must always align.
-    let layer4 = MyLayer { layout: layout![1], blocks: vec![MyBlock::new()] };
-    // Actually, try to not do that at all.
-
-    // Here are some basic ways to stack layers.
-    let mut pyramid = MyStack::new();
-    pyramid.stack_all(vec![layer1, layer3]);
-    pyramid.insert(1, layer2);
-    pyramid.stack(layer4);
-
-    // Pyramid layers are misaligned, so let's center it using offsets.
-    let mut count = 0usize;
-    for layer in pyramid.layers_mut().iter_mut() {
-        layer.offset_x(count);
-        layer.offset_y(count);
-        count += 1
+impl Block for MyBlock {
+    fn void() -> Self {
+        MyBlock {
+            id: String::new(),
+            connections: Vec::new()
+        }
     }
 
-    // And that's a pyramid! Have fun trying to pretty-print it!
-    // Next up in example 2, learn how to connect blocks using procedural generation.
+    fn is_void(&self) -> bool {
+        match &self.id {
+            "" => true,
+            _ => false
+        }
+    }
+
+    type ConnectionInstructions = ();
+
+    fn connect(&mut self, other: &mut Self, _instr: ()) {
+        self.connections.push(other.id)
+    }
 }
+
+struct MyStack {
+    layouts: Vec<Layout>,
+    blocks: Vec<MyBlock>
+}
+impl Stack<MyBlock> for MyStack {
+    fn new() -> Self {
+        MyStack { layouts: vec![], blocks: vec![] }
+    }
+    fn layouts(&self) -> &Vec<Layout> { &self.layouts }
+    fn layouts_mut(&mut self) -> &mut Vec<Layout> { &mut self.layouts }
+    fn blocks(&self) -> &Vec<Block> { &self.blocks }
+    fn blocks_mut(&mut self) -> &mut Vec<Block> { &mut self.blocks }
+}
+
+fn main() {}
